d30c5e5093e2174b066e2f114afdd496d6b99dd9
Rossvet <dmitriy.marvis@gmail.com>
2020-10-22 11:45:24 +0300

    Add form validation and styles gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkTextInputs from "./modules/checkTextInputs";
+import showMoreStyles from "./modules/showMoreStyles";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -10,6 +13,11 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', ' ', '.main-prev-btn', '.main-next-btn');
 
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+
+    showMoreStyles('.button-styles', '.styles-2');
 
 });
 
